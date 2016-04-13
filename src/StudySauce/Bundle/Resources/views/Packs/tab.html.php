460478c6462deca3040468649945a445dd726cd9
bjcullinan <code@bjcullinan.com>
2016-04-13 01:04:58 -0700

    pack list fix

--- src/StudySauce/Bundle/Resources/views/Packs/tab.html.php
+++ src/StudySauce/Bundle/Resources/views/Packs/tab.html.php
@@ -49,7 +49,7 @@ $view['slots']->start('body'); ?>
                 $tables['count-card'] = empty($entity->getId()) ? 5 : 0;
             }
             else {
-                $tables['pack-count'] = 0;
+                $tables['count-pack'] = 0;
                 $tables['tables'] = ['pack' => ['id' => ['created', 'id'], 'name' => ['title','userCountStr','cardCountStr'], 'packList' => ['groups', 'userPacks.user'], 'actions' => ['status']]];
                 $tables['classes'] = ['tiles'];
                 $tables['headers'] = ['pack' => 'new'];
