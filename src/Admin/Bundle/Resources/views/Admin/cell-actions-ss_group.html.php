17d8ba18a7507b5198208e2cf34d68ed5085f6c0
bjcullinan <code@bjcullinan.com>
2016-05-24 10:00:27 -0700

    no message

--- src/Admin/Bundle/Resources/views/Admin/cell-actions-ss_group.html.php
+++ src/Admin/Bundle/Resources/views/Admin/cell-actions-ss_group.html.php
@@ -5,7 +5,10 @@
     if(isset($request['pack-id']) && !empty($packId = $request['pack-id'])) { ?>
         <a href="#general-dialog" data-confirm="Are you sure you would like to remove the group &ldquo;<?php print ($ss_group->getName()); ?>&rdquo; from the pack?" class="remove-icon" data-action="<?php print ($view['router']->generate('save_group', ['ss_group' => ['id' => $ss_group->getId(), 'groupPacks' => ['id' => $packId, 'remove' => 'true']]])); ?>" data-target="#general-dialog" data-toggle="modal">&nbsp;</a>
     <?php }
-    else { ?>
-        <a href="#general-dialog" data-confirm="Are you sure you would like to delete the group &ldquo;<?php print ($ss_group->getName()); ?>&rdquo; permanently?" class="remove-icon" data-action="<?php print ($view['router']->generate('save_group', ['ss_group' => ['id' => $ss_group->getId(), 'deleted' => '1']])); ?>" data-target="#general-dialog" data-toggle="modal">&nbsp;</a>
+    else {
+        $removeParams = ['id' => $ss_group->getId(), 'deleted' => '1'];
+
+        ?>
+        <a href="#general-dialog" data-confirm="Are you sure you would like to delete the group &ldquo;<?php print ($ss_group->getName()); ?>&rdquo; permanently?" class="remove-icon" data-action="<?php print ($view['router']->generate('save_group', ['ss_group' => $removeParams])); ?>" data-target="#general-dialog" data-toggle="modal">&nbsp;</a>
     <?php } ?>
 </div>
