a7c8e17bd9c361d9ee3314b549d0cfa9f03a4f83
bjcullinan <code@bjcullinan.com>
2016-07-27 13:22:15 -0700

    no message

--- cron.sh
+++ cron.sh
@@ -6,34 +6,35 @@ echo "$(myip)"
 if  dig www.studysauce.com | grep '^[^;].*IN\sA' | grep "$myip" ; then
 echo "This is production"
 if ps -ef | grep -v grep | grep \/\/studysauce ; then
-    goto end;
+    goto end
 else
     wget --no-check-certificate -O /dev/null -o /dev/null https://studysauce.com/cron &
-    goto end;
+    goto end
 fi
 fi
 
 
 if  dig test.studysauce.com | grep '^[^;].*IN\sA' | grep "$myip" ; then
 echo "This is test"
-if ps -ef | grep -v grep | grep test\.studysauce ; then
-        goto testCron;
-else
-        wget --no-check-certificate -O /dev/null -o /dev/null https://test.studysauce.com/cron &
-        goto testCron;
-fi
 
 # check to see if cron validation is running, which is always should be
-testCron:
-if ps -ef | grep -v grep | grep test\.studysauce | grep cron\/validate ; then
-        goto end;
+if ps -ef | grep -v grep | grep cron\/validate ; then
+        goto test
 else
         cd /var/www/studysauce3/
         if ! git pull | grep "Already up to date" ; then
             ./update_test.sh
         fi
         wget --no-check-certificate -O /dev/null -o /dev/null https://test.studysauce.com/cron/validate &
-        goto end;
+        goto test
+fi
+
+test:
+if ps -ef | grep -v grep | grep test\.studysauce ; then
+        goto end
+else
+        wget --no-check-certificate -O /dev/null -o /dev/null https://test.studysauce.com/cron &
+        goto end
 fi
 fi
 
@@ -41,10 +42,10 @@ fi
 if  dig staging.studysauce.com | grep '^[^;].*IN\sA' | grep "$myip" ; then
 echo "This is staging"
 if ps -ef | grep -v grep | grep staging\.studysauce ; then
-        goto end;
+        goto end
 else
         wget --no-check-certificate -O /dev/null -o /dev/null https://staging.studysauce.com/cron &
-        goto end;
+        goto end
 fi
 fi
 
@@ -52,10 +53,10 @@ fi
 if  dig cerebro.studysauce.com | grep '^[^;].*IN\sA' | grep "$myip" ; then
 echo "This is cerebro"
 if ps -ef | grep -v grep | grep cerebro\.studysauce ; then
-        goto end;
+        goto end
 else
         wget --no-check-certificate -O /dev/null -o /dev/null https://cerebro.studysauce.com/cron &
-        goto end;
+        goto end
 fi
 fi
 
