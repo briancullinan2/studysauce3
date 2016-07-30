dded28335c6d7f713da9df5712d26ac1960ab2dd
bjcullinan <code@bjcullinan.com>
2016-07-30 11:59:29 -0700

    no message

--- src/Admin/Bundle/Tests/acceptance/3StoreCest.php
+++ src/Admin/Bundle/Tests/acceptance/3StoreCest.php
@@ -70,6 +70,7 @@ class StoreCest
         $pack = $I->grabTextFrom('//label[contains(.,"TestPack")]');
         $freePack = $I->grabFrom('StudySauceBundle:Pack', ['title' => $pack]);
         $coupon = new Coupon();
+        $coupon->setDescription('');
         $coupon->addPack($freePack);
         $coupon->setOptions(['ST' => ['price' => 0.0]]);
         $coupon->setName('Study Sauce ' . $freePack->getTitle());
