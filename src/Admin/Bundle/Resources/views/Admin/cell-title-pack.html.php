c9b17b23ac36ba51bfb64bf4c4838e13c8a416a6
bjcullinan <code@bjcullinan.com>
2016-09-23 08:02:13 -0700

    no message

--- src/Admin/Bundle/Resources/views/Admin/cell-title-pack.html.php
+++ src/Admin/Bundle/Resources/views/Admin/cell-title-pack.html.php
@@ -5,19 +5,20 @@ use StudySauce\Bundle\Entity\Pack;
 use StudySauce\Bundle\Entity\User;
 
 /** @var Pack $pack */
+$userCount = 0;
+foreach($pack->getUsers()->toArray() as $u) {
+    /** @var User $u */
+    if (!empty($up = $pack->getUserPack($u))) {
+        $userCount += !empty($up->getDownloaded()) ? 1 : 0;
+    }
+}
 
 if (isset($request['pack-id']) && $pack->getId() == $request['pack-id']) {
-    print ($view->render('AdminBundle:Admin:cell-label.html.php', ['fields' => ['All users (not in subgroups below)', 0, 0]]));
+    print ($view->render('AdminBundle:Admin:cell-label.html.php', ['fields' => ['All users (not in subgroups below)', $userCount, 0]]));
 } else { ?>
     <a href="<?php print ($view['router']->generate('packs_edit', ['pack' => $pack->getId()])); ?>">
     <?php
-    $userCount = 0;
-    foreach($pack->getUsers()->toArray() as $u) {
-        /** @var User $u */
-        if (!empty($up = $pack->getUserPack($u))) {
-            $userCount += !empty($up->getDownloaded()) ? 1 : 0;
-        }
-    }
+
     if (isset($request['ss_group-id']) && !empty($group = $request['ss_group-id'])) {
         $userGroupCount = 0;
         foreach($pack->getUsers()->toArray() as $u) {
